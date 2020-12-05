03ae32880edfe0afbf9601ec2b518cc3875b234b
Nathan Lecorbeiller <lecorbeiller.nathan@outlook.fr>
2020-12-05 16:30:16 +0100

    Added basic Movement and idle animation, also changed movement script

--- LabDay/Assets/Script/Player/PlayerController.cs
+++ LabDay/Assets/Script/Player/PlayerController.cs
@@ -4,63 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed; //Speed value
-    private bool isMoving; //Boolean to know if the player is currently moving
-    private Vector3 origPos, targetPos; //Vector that we'll use to know what direction has been choosen
-    private float timeToMove = 0.2f;
+    public float moveSpeed; // Speed value
+    private bool isMoving; // To know if the player is currently moving
+    private Vector2 input; // For getting the Input
+    private Vector2 currentDir; // For storing direction
+
+    private Animator animator;
+
+    //With this void Awake, we set the Animator so it plays the animation of the direction the player asked
+    private void Awake()
+    {
+        animator = GetComponent<Animator>(); //We set the Animator as our variable animator
+    }
 
     private void Update()
     {
-        //While the player is not moving, we read the input, and move the player in the good direction
         if (!isMoving)
         {
-            if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow))
-            {
-                StartCoroutine(MovePlayer(Vector3.up));
-            }
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
 
-            else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
-            {
-                StartCoroutine(MovePlayer(Vector3.left));
-            }
+            //Get rid of Diagonal movement
+            if (input.x != 0) input.y = 0;
+            else if (input.y != 0) input.x = 0;
 
-            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            //While the player is not moving, we read the input, and move the player in the choosen direction
+            if (input != Vector2.zero)
             {
-                StartCoroutine(MovePlayer(Vector3.down));
-            }
+                animator.SetFloat("moveX", input.x); //Link the moveX from the animator with the input.x of the code
+                animator.SetFloat("moveY", input.y); //Same for the Y
 
-            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            {
-                StartCoroutine(MovePlayer(Vector3.right));
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
             }
         }
+        animator.SetBool("isMoving", isMoving); //Link the Animator "isMoving" to the script "isMoving"
     }
 
-    void GoUp()
-    { }
-
     //This coroutine get us a smooth movement
-    private IEnumerator MovePlayer(Vector3 direction)
+    IEnumerator Move(Vector3 targetPos)
     {
         //We set isMoving to true in the beggining and false at the end
         isMoving = true;
 
-        float elapsedTime = 0;
-
-        origPos = transform.position;
-        targetPos = origPos + direction;
-
         //this part get us a smooth movement, instead of juste moving tile to tile
-        while (elapsedTime < timeToMove)
+        while ((targetPos - transform.position).sqrMagnitude >Mathf.Epsilon)
         {
-            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
-            elapsedTime += Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         }
-
-        //We want to be sure that in the end, the player is on the targetPos, and this will rarely occur, this will just happen on lags
-        transform.position = targetPos;
-
         isMoving = false;
     }
 }
