8bfee7b6284e39c776e91ff0abee892571e3c973
Nathan Lecorbeiller <lecorbeiller.nathan@outlook.fr>
2020-12-12 14:00:53 +0100

    We can now selectr an action, and a move!
    Added a DialogSystem, evolving within the battle in real time, we can choose between some actions, then some moves.
    
    Also changed some settings in the Sprites to make it look Better

---
+++ LabDay/Assets/Script/Battle/BattleDialogBox.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //We use this library bc we actually work with UI
+
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] Color highlightedColor;
+
+
+    [SerializeField] Text dialogText;
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts; //We use List bc we want the player to choose between several actions
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+    //This function will set the dialog when we call it
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+    }
+
+    //This coroutine will make it looks like the dialog is writing ahead.
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray()) //For each letter in our dialog text
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond); //We can choose how many letter per second we want, in Unity itself
+        }
+    }
+    //This will change wich dialog we should see, either Dialog, Move selector, etc
+    public void EnableDialogText (bool enabled) 
+    {
+        dialogText.enabled = enabled;
+    }
+    public void EnableActionSelector (bool enabled)
+    {
+        actionSelector.SetActive(enabled); //Since actionSelector is a GameObject and not a text, we have to use the SetActive() function
+    }
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled); //Since moveSelector is a GameObject and not a text, we have to use the SetActive() function
+        moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction) //This will actually change the color of the actionSelector selected
+    {
+        for (int i=0; i<actionTexts.Count; ++i)
+        {
+            if (i == selectedAction)
+                actionTexts[i].color = highlightedColor;     
+            else
+                actionTexts[i].color = Color.black;
+        }
+    }
+
+    public void UpdateMoveSelection(int selectedMove, Move move) //This function is the same as UpdateActionSelection, but for the moves
+    {
+        for (int i=0; i<moveTexts.Count; ++i)
+        {
+            if (i == selectedMove)
+                moveTexts[i].color = highlightedColor;
+            else
+                moveTexts[i].color = Color.black;
+        }
+
+        ppText.text = $"PP {move.PP}/{move.Base.Pp}"; //We show how many Pp are lefts
+        typeText.text = move.Base.Type.ToString(); //We also convert our enumType in a String, to show wich type is the actual Move
+    }
+
+    public void SetMoveNames(List<Move> moves)
+    {
+        //Loop throught our MovesText
+        for (int i=0; i<moveTexts.Count; ++i) //We usually have 4 mouves, but sometimes we can have less
+        {
+            if (i < moves.Count)
+            {
+                moveTexts[i].text = moves[i].Base.Name; //Set the move in our Selector, to the name of our move
+            }
+            else
+                moveTexts[i].text = "-";
+        }
+    }
+}
--- LabDay/Assets/Script/Battle/BattleSystem.cs
+++ LabDay/Assets/Script/Battle/BattleSystem.cs
@@ -2,23 +2,126 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Here is where we manage our battle system, by calling every needed function, that we create in other classes
+
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy} //We will use different state in our BattleSystem, and show what need to be shown in a specific state
+
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
+
+    BattleState state;
+    int currentAction; //Actually, 0 is Fight, 1 is Run
+    int currentMove; //We'll have 4 moves
+
     //We want to setup everything at the very first frame of the battle
     private void Start()
     {
-        SetupBattle(); //We call our SetupBattle function
+        StartCoroutine(SetupBattle()); //We call our SetupBattle function
     }
 
-    public void SetupBattle() //We use the data created in the BattleUnit and BattleHud scripts
+    public IEnumerator SetupBattle() //We use the data created in the BattleUnit and BattleHud scripts
     {
         playerUnit.Setup();
         enemyUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
         enemyHud.SetData(enemyUnit.Pokemon);
+
+        dialogBox.SetMoveNames(playerUnit.Pokemon.Moves);
+
+        //We return the function Typedialog
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared."); //With the $, a string can show a special variable in it
+        yield return new WaitForSeconds(1f);
+
+        //This is the function where the player choose a specific action
+        PlayerAction(); 
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction; //Change the state to PlayerAction
+        StartCoroutine(dialogBox.TypeDialog("Choose an action")); //Then write a text
+        dialogBox.EnableActionSelector(true); //Then allow player to choose an Action
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove; //Change the state to PlayerMove
+        dialogBox.EnableActionSelector(false); //Then disable player to choose an action, and allow it to choose a move
+        dialogBox.EnableDialogText(false); //Disable the DialogText
+        dialogBox.EnableMoveSelector(true); //Enable the MoveSelector
+    }
+
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection(); //Function to make the player able to choose an action
+        }
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow)) //For now we have two choices so we just want to increase or decrease the int currentAction
+        {
+            if (currentAction == 0)
+                ++currentAction; 
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction == 1)
+                --currentAction;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentAction == 0)
+            {
+                //Fight*
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                //Run
+            }
+        }
+    }
+
+
+    //We create a way to move freely beyond every moves our Creature actually has.
+    void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 1)
+                ++currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentMove > 0)
+                --currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) //The left and right keys moves by one, but the Up and Down have to move by 2
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 2)
+                currentMove += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentMove > 1)
+                currentMove -= 2;
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Pokemon.Moves[currentMove]);
     }
 }
